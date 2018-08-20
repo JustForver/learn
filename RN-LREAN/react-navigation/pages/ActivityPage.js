7195ccb35fb41c89e9acae717d280b0f5c0190f5
osx <154129423@qq.com>
2018-08-20 11:37:35 +0800

    gz

---
+++ RN-LREAN/react-navigation/pages/ActivityPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {
+    Text
+} from 'react-native';
+
+export default class Activity extends Component {
+    render(){
+        return(
+            <Text>Welcome to Activity page!</Text>
+        )
+    }
+}
+
---
+++ RN-LREAN/react-navigation/pages/LinePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+    Text
+} from 'react-native';
+
+
+
+export default class Line extends Component {
+    render(){
+        return(
+            <Text>Welcome to Line page!</Text>
+        )
+    }
+}
+
